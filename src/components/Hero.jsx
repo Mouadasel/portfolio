82f5f418c3e9cace0cd12d361e6f74ade53347b4
Admin <admin@Admins-MacBook-Pro.local>
2025-02-13 12:15:06 +0100

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -7,6 +9,17 @@ const Hero = () => {
             <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Mouad Assel
             </h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              Front End Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justifly-center">
+            <img src={profilePic} alt="mouad assel" />
           </div>
         </div>
       </div>
