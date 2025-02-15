172622acc33cca166f934fefd959de3c0291fc0a
Admin <admin@Admins-MacBook-Pro.local>
2025-02-15 12:49:22 +0100

    use framer motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,38 @@
 import { CONTACT } from "../constants";
-
-const  Contact =()=>{
-    return (
-        <div className="border-b border-neutral-900 pb-20">
-            <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
-            <div className="text-center tracking-tighter">
-                <p className="my-4">{CONTACT.address}</p>
-                <p className="my-4">{CONTACT.phoneNo}</p>
-                <a href="#" className="border-b">{CONTACT.email}</a>  
-            </div>
-        </div>
-    );
-}
+import { motion } from "framer-motion";
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <motion.h2
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h2>
+      <div className="text-center tracking-tighter">
+        <motion.p
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <a href="#" className="border-b">
+          {CONTACT.email}
+        </a>
+      </div>
+    </div>
+  );
+};
 export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,24 @@
 import { PROJECTS } from "../constants";
-
+import { motion } from "framer-motion";
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pd-4">
-      <h2 className="my-20 text-center text-4xl ">Projects</h2>
+      <motion.h2
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: .5 }}
+        className="my-20 text-center text-4xl "
+      >
+        Projects
+      </motion.h2>
       {PROJECTS.map((project, index) => (
         <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-          <div className="mb-2 w-full lg:w-1/4">
+          <motion.div
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: .5 }}
+            className="mb-2 w-full lg:w-1/4"
+          >
             <img
               src={project.image}
               alt={project.title}
@@ -14,12 +26,15 @@ const Projects = () => {
               height={150}
               className="mb-6 rounded"
             />
-          </div>
-          <div className="w-full max-w-xl lg:w-3/4">
+          </motion.div>
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: .5 }}
+            className="w-full max-w-xl lg:w-3/4"
+          >
             <h6 className="mb-2 font-semibold">{project.title}</h6>
-            <p className="mb-4 text-neutral-400">
-              {project.description}
-            </p>
+            <p className="mb-4 text-neutral-400">{project.description}</p>
             {project.technologies.map((tech, index) => (
               <span
                 key={index}
@@ -28,7 +43,7 @@ const Projects = () => {
                 {tech}
               </span>
             ))}
-          </div>
+          </motion.div>
         </div>
       ))}
     </div>
